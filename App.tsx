bd686c1698df33e26cdf4887c8669cb742efac01
Matt Wood <matt@mattwood.co>
2021-12-22 16:51:17 -0700

    working navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
 import { StatusBar } from "expo-status-bar";
-import { withAuthenticator } from "aws-amplify-react-native";
-import { StyleSheet, Text, View } from "react-native";
-import Amplify from "aws-amplify";
-import config from "./aws-exports";
+// import { withAuthenticator } from "aws-amplify-react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
+// import Amplify from "aws-amplify";
+// import config from "./aws-exports";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-Amplify.configure(config);
+// Amplify.configure(config);
 
-function App() {
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Other"
+        onPress={() => navigation.navigate("Other")}
+      />
     </View>
   );
 }
-export default withAuthenticator(App);
+function OtherScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>OtherScreen Screen</Text>
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+    </View>
+  );
+}
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Other" component={OtherScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+export default App;
 
 const styles = StyleSheet.create({
   container: {
