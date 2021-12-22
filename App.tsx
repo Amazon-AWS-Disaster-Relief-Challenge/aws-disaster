84aedbb9d5e135d347ada35abf7c2cd063130d9f
Matt Wood <matt@mattwood.co>
2021-12-22 13:56:05 -0700

    init

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { withAuthenticator } from "aws-amplify-react-native";
+import { StyleSheet, Text, View } from "react-native";
+import Amplify from "aws-amplify";
+import config from "./aws-exports";
 
-export default function App() {
+Amplify.configure(config);
+
+function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -9,12 +14,13 @@ export default function App() {
     </View>
   );
 }
+export default withAuthenticator(App);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "awsdisasterhackathon": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "awsdisasterhackathon": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "geo": {
+        "mape3c603a1": {
+            "Name": "string",
+            "Style": "string",
+            "Region": "string",
+            "Arn": "string"
+        }
+    }
+}
