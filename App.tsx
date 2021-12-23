5e926a05ad4e385366ce051122ad128ad2f8b687
Matt Wood <matt@mattwood.co>
2021-12-22 17:14:33 -0700

    working video

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
-import { StatusBar } from "expo-status-bar";
+import * as React from "react";
+// import { StatusBar } from "expo-status-bar";
 // import { withAuthenticator } from "aws-amplify-react-native";
 import { Button, StyleSheet, Text, View } from "react-native";
 // import Amplify from "aws-amplify";
@@ -7,6 +8,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // Amplify.configure(config);
+import { Video } from "expo-av";
+import VideoPlayer from "expo-video-player";
 
 function HomeScreen({ navigation }) {
   return (
@@ -20,13 +23,36 @@ function HomeScreen({ navigation }) {
   );
 }
 function OtherScreen({ navigation }) {
+  const videoPlayerRef = React.useRef();
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>OtherScreen Screen</Text>
+      {/* <Text>OtherScreen Screen</Text> */}
       <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+
+      <VideoPlayer
+        videoProps={{
+          shouldPlay: true,
+          resizeMode: Video.RESIZE_MODE_CONTAIN,
+          // ❗ source is required https://docs.expo.io/versions/latest/sdk/video/#props
+          source: {
+            uri: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+          },
+        }}
+      />
     </View>
   );
 }
+
+// Later on in your styles..
+var videoStyles = StyleSheet.create({
+  backgroundVideo: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+});
 const Stack = createNativeStackNavigator();
 
 function App() {
